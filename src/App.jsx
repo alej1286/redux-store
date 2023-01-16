6f5c1289517eaa0156628cdf50ab5b4ff51d770d
Juan Alejandro Work <alej1286@gmail.com>
2023-01-16 15:55:04 -0500

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Hero from './components/Hero'
-import { heroapi } from './data/data'
+import React from 'react';
+import { Cart, FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components';
+import { heroapi, popularsales, toprateslaes, highlight, sneaker, story, footerAPI } from './data/data'
 
 
 const App = () => {
@@ -8,7 +8,17 @@ const App = () => {
     <>
       <main>
         
-        <Hero heroapi={heroapi}/>
+      <Navbar/>
+      <Cart />
+      <main className='flex flex-col gap-16 relative'>
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
       </main>
     </>
   )
